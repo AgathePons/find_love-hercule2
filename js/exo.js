0e8b78481fbd3eefecdc28c47fdfaba3a696e9a9
AgathePons <ponsagathe37@gmail.com>
2021-11-29 08:40:12 +0100

    eslint rules + steps 1, 2, 3

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,19 @@
-console.log('test 1...2...1...2');
+const app = {
+  hercule: {
+    name: 'Hercule',
+    job: 'Demi-dieu',
+    age: 35,
+    department: 75,
+    arm: 60.5,
+    inRealtionship: true,
+  },
+  friends: ['Jupiter', 'Junon', 'Alcmène', 'Déjanire'],
+
+  init: () => {
+    base.fillProfil(app.hercule);
+    base.printFriends(app.friends);
+    base.setBestFriend(app.friends[0]);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
