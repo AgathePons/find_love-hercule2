485285c04f12028a2ebbe07dd25a082c895213d4
AgathePons <ponsagathe37@gmail.com>
2021-11-29 10:03:01 +0100

    steps 7, 8, 9

--- js/exo.js
+++ js/exo.js
@@ -8,18 +8,38 @@ const app = {
     inRealtionship: true,
   },
   friends: ['Jupiter', 'Junon', 'Alcmène', 'Déjanire'],
-
+  
   init: () => {
     app.printBannerTitle();
+    app.displayProfilName(app.hercule.name, app.hercule.department);
     app.displayWorks();
     app.displayDisponibilty();
     base.fillProfil(app.hercule);
     base.printFriends(app.friends);
     base.setBestFriend(app.friends[0]);
     base.displayWork(2);
+    
+    // menu
+    const menuToggler = document.getElementById('menu-toggler');
+    menuToggler.addEventListener('click', function() {
+      const headerBanner = document.getElementById('header-banner');
+      headerBanner.classList.toggle('banner--open');
+    });
+
+    // chat
+    const contactForm = document.getElementById('contact');
+    //const inputForm = contactForm.querySelector('.contact__input');
+    const inputButton = contactForm.querySelector('.contact__button');
+    inputButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      alert('Hercule ne souhaite pas être dérangé');
+    });
   },
 
   // ------ Methods ------ //
+  /**
+   * Create and fill H1 title
+   */
   printBannerTitle: () => {
     const bannerTitle = document.createElement('h1');
     const headerBanner = document.getElementById('header-banner');
@@ -35,6 +55,9 @@ const app = {
       base.displayWork(i);
     }
   },
+  /**
+   * Get the current hour and display disponibility
+   */
   displayDisponibilty: () => {
     const availabilityElement = document.getElementById('availability');
     let currentHour = base.getHour();
@@ -48,6 +71,24 @@ const app = {
       availabilityElement.textContent = 'Non disponible';
       availabilityElement.classList.add('off');
     }
+  },
+  /**
+   * display the created pseudo in #profil-name
+   * @param {string} firstName 
+   * @param {number} departmentNumber 
+   */
+  displayProfilName: (firstName, departmentNumber) => {
+    const profilNameTitle = document.getElementById('profil-name');
+    profilNameTitle.textContent = app.createPseudo(firstName, departmentNumber);
+  },
+  /**
+   * create and return the pseudo
+   * @param {string} firstName
+   * @param {number} departmentNumber
+   * @returns {string} pseudo
+   */
+  createPseudo: (firstName, departmentNumber) => {
+    return `${firstName}-du-${departmentNumber}`;
   },
 };
 
